499c8f19200fdef8c31f3ee16d03947f5950d405
v010D <vbaryshenskiy@gmail.com>
2023-09-14 21:14:08 -0400

    Input a Modifier User can now add a flat modifier to their roll Fixed some issues that can arise with blank input box or modifier being a decimal

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
 /*  Function to simulate dice rolling
-TODO: Needs to roll more than one dice !!DONE!!
-TODO: Needs to be able to add a static user input number to total roll
-TODO: Needs to display each individual result when user rolls more than once (this should have a scrolling feature to see everything)
-TODO (CONT) this thing might be better served to be in some dialogue box that saves a certain amount of rolls for history
-TODO: ADVANCED!!! Each set of dice should be an object for each individual that is associated to the person who pressed the button
 */
 
 function randomNumberGenerator(maximum) {
@@ -25,30 +20,38 @@ resultElement# displays the result of a roll
 
 const rollButton4 = document.getElementById('rollButton4');
 const resultElement4 = document.getElementById('result4');
+const staticNum4 = document.getElementById('staticNum4'); // Get the static number input element
 const clearButton4 = document.getElementById('clearButton4');
+
 
 const rollButton6 = document.getElementById('rollButton6');
 const resultElement6 = document.getElementById('result6');
+const staticNum6 = document.getElementById('staticNum6'); // Get the static number input element
 const clearButton6 = document.getElementById('clearButton6');
 
 const rollButton8 = document.getElementById('rollButton8');
 const resultElement8 = document.getElementById('result8');
+const staticNum8 = document.getElementById('staticNum8'); // Get the static number input element
 const clearButton8 = document.getElementById('clearButton8');
 
 const rollButton10 = document.getElementById('rollButton10');
 const resultElement10 = document.getElementById('result10');
+const staticNum10 = document.getElementById('staticNum10'); // Get the static number input element
 const clearButton10 = document.getElementById('clearButton10');
 
 const rollButton100 = document.getElementById('rollButton100');
 const resultElement100 = document.getElementById('result100');
+const staticNum100 = document.getElementById('staticNum100'); // Get the static number input element
 const clearButton100 = document.getElementById('clearButton100');
 
 const rollButton12 = document.getElementById('rollButton12');
 const resultElement12 = document.getElementById('result12');
+const staticNum12 = document.getElementById('staticNum12'); // Get the static number input element
 const clearButton12 = document.getElementById('clearButton12');
 
 const rollButton20 = document.getElementById('rollButton20');
 const resultElement20 = document.getElementById('result20');
+const staticNum20 = document.getElementById('staticNum20'); // Get the static number input element
 const clearButton20 = document.getElementById('clearButton20');
 
 // Event listeners to handle button clicks
@@ -56,7 +59,8 @@ const clearButton20 = document.getElementById('clearButton20');
 rollButton4.addEventListener('click', () => {
     const numDice4 = parseInt(document.getElementById('numDice4').value);
     const rollResults4 = rollMultipleDice(4, numDice4);
-    const totalRoll4 = rollResults4.reduce((total, roll) => total + roll, 0);
+    const staticNumber = staticNum4.value === '' ? 0 : parseInt(staticNum4.value);
+    const totalRoll4 = rollResults4.reduce((total, roll) => total + roll, 0) + staticNumber;
     resultElement4.textContent = `Total: ${totalRoll4}`;
 });
 
@@ -67,7 +71,8 @@ clearButton4.addEventListener('click', () => {
 rollButton6.addEventListener('click', () => {
     const numDice6 = parseInt(document.getElementById('numDice6').value);
     const rollResults6 = rollMultipleDice(6, numDice6);
-    const totalRoll6 = rollResults6.reduce((total, roll) => total + roll, 0);
+    const staticNumber = staticNum6.value === '' ? 0 : parseInt(staticNum6.value);
+    const totalRoll6 = rollResults6.reduce((total, roll) => total + roll, 0) + staticNumber;
     resultElement6.textContent = `Total: ${totalRoll6}`;
 });
 
@@ -78,7 +83,8 @@ clearButton6.addEventListener('click', () => {
 rollButton8.addEventListener('click', () => {
     const numDice8 = parseInt(document.getElementById('numDice8').value);
     const rollResults8 = rollMultipleDice(8, numDice8);
-    const totalRoll8 = rollResults8.reduce((total, roll) => total + roll, 0);
+    const staticNumber = staticNum8.value === '' ? 0 : parseInt(staticNum8.value);
+    const totalRoll8 = rollResults8.reduce((total, roll) => total + roll, 0) + staticNumber;
     resultElement8.textContent = `Total: ${totalRoll8}`;
 });
 
@@ -89,7 +95,8 @@ clearButton8.addEventListener('click', () => {
 rollButton10.addEventListener('click', () => {
     const numDice10 = parseInt(document.getElementById('numDice10').value);
     const rollResults10 = rollMultipleDice(10, numDice10);
-    const totalRoll10 = rollResults10.reduce((total, roll) => total + roll, 0);
+    const staticNumber = staticNum10.value === '' ? 0 : parseInt(staticNum10.value); // Get the static number from the input
+    const totalRoll10 = rollResults10.reduce((total, roll) => total + roll, 0) + staticNumber;
     resultElement10.textContent = `Total: ${totalRoll10}`;
 });
 
@@ -100,7 +107,8 @@ clearButton10.addEventListener('click', () => {
 rollButton100.addEventListener('click', () => {
     const numDice100 = parseInt(document.getElementById('numDice100').value);
     const rollResults100 = rollMultipleDice(100, numDice100);
-    const totalRoll100 = rollResults100.reduce((total, roll) => total + roll, 0);
+    const staticNumber = staticNum100.value === '' ? 0 : parseInt(staticNum100.value); // Get the static number from the input
+    const totalRoll100 = rollResults100.reduce((total, roll) => total + roll, 0) + staticNumber;
     resultElement100.textContent = `Total: ${totalRoll100}`;
 });
 
@@ -111,7 +119,8 @@ clearButton100.addEventListener('click', () => {
 rollButton12.addEventListener('click', () => {
     const numDice12 = parseInt(document.getElementById('numDice12').value);
     const rollResults12 = rollMultipleDice(12, numDice12);
-    const totalRoll12 = rollResults12.reduce((total, roll) => total + roll, 0);
+    const staticNumber = staticNum12.value === '' ? 0 : parseInt(staticNum12.value); // Get the static number from the input
+    const totalRoll12 = rollResults12.reduce((total, roll) => total + roll, 0) + staticNumber;
     resultElement12.textContent = `Total: ${totalRoll12}`;
 });
 
@@ -122,7 +131,8 @@ clearButton12.addEventListener('click', () => {
 rollButton20.addEventListener('click', () => {
     const numDice20 = parseInt(document.getElementById('numDice20').value);
     const rollResults20 = rollMultipleDice(20, numDice20);
-    const totalRoll20 = rollResults20.reduce((total, roll) => total + roll, 0);
+    const staticNumber = staticNum20.value === '' ? 0 : parseInt(staticNum20.value); // Get the static number from the input
+    const totalRoll20 = rollResults20.reduce((total, roll) => total + roll, 0) + staticNumber;
     resultElement20.textContent = `Total: ${totalRoll20}`;
 });
 
